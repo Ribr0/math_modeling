fe6f9004b569cf2f7efc7611240574edfa8a839e
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2021-11-24 18:49:29 +0000

    сделал первую лабу

---
+++ lec_1_type.py
@@ -0,0 +1,21 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('Компьютер'))
+
+a = 'Molodec'
+print(type(a))
+
+a = ['Molodec', 1, 'Ккрто', 6, 8]
+print (type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
