a057d0420e1fd624ced81b88421ac540caedbe47
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2021-12-01 17:47:51 +0000

    вторая лаба

---
+++ lab_2_doptask3.py
@@ -0,0 +1,11 @@
+a = int(input('Введите число:'))
+
+b = a % 10
+print(b)
+
+
+c = a / 10
+print(c)
+
+d = c // 10
+print(c)
---
+++ lab_2_task4.py
@@ -0,0 +1,18 @@
+a = int(input('Введите число:'))
+
+x = 1
+
+b = 1
+
+c = 1
+
+print(1)
+
+print(1)
+
+while x < a:
+  x = x + 1
+  d = b + c
+  print(d)
+  b = c
+  c = d
