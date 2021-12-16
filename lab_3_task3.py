d0b754f21e996c0c0c9c4dd66aa0e175b59ce3a7
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2021-12-16 10:52:36 +0000

    lab 3

--- lab_3_task3.py
+++ lab_3_task3.py
@@ -3,7 +3,37 @@ from lab_3_const import svobod_pad as g
 x0 = int(input('x0 = '))
 y0 = int(input('y0 = '))
 V0 = int(input('V0 = '))
-t = int(input('t = '))
-x = x0+V0*t
-y = (y0+V0*t-((g*(t**2)/2)))
-print(f'{x}, {y}')
+
+t0 = 0
+x0S = x0+V0*t0
+y0S = (y0+V0*t0-((g*(t0**2)/2)))
+
+t1 = 1
+x1S = x0+V0*t1
+y1S = (y0+V0*t1-((g*(t1**2)/2)))
+
+t2 = 2
+x2S = x0+V0*t2
+y2S = (y0+V0*t2-((g*(t2**2)/2)))
+
+t3 = 3
+x3S = x0+V0*t3
+y3S = (y0+V0*t3-((g*(t3**2)/2)))
+
+t4 = 4
+x4S = x0+V0*t4
+y4S = (y0+V0*t4-((g*(t4**2)/2)))
+
+t5 = 5
+x5S = x0+V0*t5
+y5S = (y0+V0*t5-((g*(t5**2)/2)))
+
+l = [[t0, x0S, y0S], [t1, x1S, y1S], [t2, x2S, y2S], [t3, x3S, y3S], [t4, x4S, y4S], [t5, x5S, y5S]]
+
+mas = np.array(l)
+print(mas)
+
+
+
+
+
