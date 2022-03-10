a76179e50d7be8e69c349c8917f540afe3759afa
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2022-03-10 14:51:40 +0000

    1

--- lab_6_doptask2.py
+++ lab_6_doptask2.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def elps(ex=0.3,p=3):
+def elps(p=1):
+  ex = np.arange(-1, 1, 0.01)
   phi = np.arange(-2*np.pi, 2*np.pi, 0.1)
   r = p/(1+((ex)*np.cos(phi)))
   plt.plot(r)
