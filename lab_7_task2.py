f11d1346112f5eb67b28765b191ec2ce77bfdba8
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2022-04-07 11:53:40 +0000

    1

--- lab_7_task2.py
+++ lab_7_task2.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
-krug, = plt.plot([], [], 'o', color='r')
+krug, = plt.plot([], [], 'o', color='b')
 
 xdata, ydata = [], []
 
-def circle_move():
-  x = alp * t
-  y = alp * t
+def circle_move(R):
+  alpha = np.arange(0, 2*np.pi, 0.1)
+  x = R * np.cos(alpha)
+  y = R * np.sin(alpha)
+  return x, y
 
 edge = 3
 plt.axis('equal')
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
-def update(i):
-  krug.det_data(circle_move(alp = np.arange(0, np.pi*2, 0.1), t = 20))
+t = np.arange(0, 2*np.pi, 0.1)
 
-ani = FuncAnimation(fig, update, frames=60, interval=40)
+def update(i):
+  krug.set_data(circle_move(R=0.05*i))
+
+ani = FuncAnimation(fig, update, frames=60, interval=50)
 
 ani.save('anima_task2.gif')
-
 
   
   
