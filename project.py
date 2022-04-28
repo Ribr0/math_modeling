bb4761f6da6074a156edacc802851df5aad4737d
Ribr0 <94626832+Ribr0@users.noreply.github.com>
2022-04-28 17:48:50 +0000

    project

--- project.py
+++ project.py
@@ -1,36 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import time 
-frames = 120
 
-R_v = 1 # радиус воды
-R_k = 1.5 #радиус капли
-a_v = 1 # параметр шариков воды
-a_k = 1 # параметр капли
+frames = 20
+N = 15
+fig, ax = plt.subplots()
+kaplya, = plt.plot([], [], color='k')
 
-def voda(R_v, a_v):
-  x = R_v * np.sin(a_v)
-  y = R_v * np.cos(a_v)
-  return x, y
 
-def kaplya(R_k, a_k):
-  x = R_k * np.sin(a_k)
-  y = R_k * np.cos(a_k)
-  return x, y 
+def drop(R, x0, y0, vx0, vy0, t):
+    x1 = x0 + vx0 * t
+    y1 = y0 + vy0 * t
+    alp = np.arange(0, 2*np.pi, 0.1)
+    x = x1 + R * np.sin(alp)
+    y = y1 + R * np.cos(alp)
+    return x, y
+
+water = []
+
+colors = {0: 'r', 1: 'g'}
 
 def animate(i):
-  voda.set_data(voda(R_k=1, a_v=1)
-  kaplya.set_data(kaplya(R_k=1, a_k=i))
+    if i > 5:
+        kaplya.set_data(drop(0.1, 0.5, 1, 0, 0, t=i))
+    else:
+        kaplya.set_data(drop(0.1, 0.5, 1, 0, -0.06, t=i))
+    water.clear()
+    for j in range(N):
+        if i < 5:
+            ball, = plt.plot([], [], color=colors[0])
+            water.append(ball)
+        else:
+            ball, = plt.plot([], [], color=colors[1])
+            water.append(ball)
 
-fig, ax = plt.subplots()
-voda, = plt.plot([], [], color='black')
-kaplya, = plt.plot([], [], color='r')
+    for j in range(3):
+        for k in range(5):
+            ball = water[k*2 + 3*j]
+            ball.set_data(drop(0.1, 0.1 + k*0.2, 0.5 - 0.2*j, 0, 0, t=i))
 
-ani = FuncAnimation(fig, animate, frames=frames, interval=30)
+
+ani = FuncAnimation(fig, animate, frames=frames, interval=100)
 
 plt.axis('square')
-plt.xlim(-5,5)
-plt.ylim(-5,5)
+plt.xlim(0,1)
+plt.ylim(0,1)
+
 
 ani.save('project.gif')
